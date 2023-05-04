dd95d8c758f2971d707a37f2a5bff3b009eaa7e5
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-05-04 19:48:23 +0200

    componente header

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+
+  import HeaderComp from './components/HeaderComp.vue';
+
+  export default{
+    name: "App",
+    components: {
+      HeaderComp,
+    }
+  }
+
+</script>
+
+<template>
+  <HeaderComp/>
+</template>
+
+<style lang="scss">
+  @use './style/main.scss'
+</style>
---
+++ src/components/HeaderComp.vue
@@ -0,0 +1,111 @@
+<script>
+
+     export default{
+          name: "HeaderComp",
+          data(){
+               return{
+                    menu: [
+                         {
+                              nome: 'characters',
+                              current: false
+                         },
+                         {
+                              nome: 'comics',
+                              current: true
+                         },
+                         {
+                              nome: 'movies',
+                              current: false
+                         },
+                         {
+                              nome: 'tv',
+                              current: false
+                         },
+                         {
+                              nome: 'games',
+                              current: false
+                         },
+                         {
+                              nome: 'collectibles',
+                              current: false
+                         },
+                         {
+                              nome: 'videos',
+                              current: false
+                         },
+                         {
+                              nome: 'fans',
+                              current: false
+                         },
+                         {
+                              nome: 'news',
+                              current: false
+                         },
+                         {
+                              nome: 'shop',
+                              current: false
+                         },
+
+                    ]
+               }
+          }
+     }
+
+</script>
+
+<template>
+  <header>
+     <nav>
+          <img src="/img/dc-logo.png" alt="">
+
+          <ul>
+               <li v-for="(element, index) in menu" :key="index"
+               :class="element.current ? 'active' : '' ">
+                    <a href="">{{ element.nome }}</a>
+               </li>
+          </ul>
+
+     </nav>
+  </header>
+</template>
+
+<style scoped lang="scss">
+     header{
+          height: 100px;
+          nav{
+               display: flex;
+               justify-content: space-between;
+               align-items: center;
+               width: 80%;
+               margin: 0 auto;
+               padding: 20px;
+               ul{
+                    display: flex;
+                    li{
+                         list-style: none;
+                         margin: 0 10px;
+                         line-height: 100px;
+                         a{
+                              text-decoration: none;
+                              color: black;
+                              text-transform: uppercase; 
+                         }
+                         &.active {
+                              border-bottom: 2px solid blue; 
+                         }
+                         &.active a {
+                              color: blue;
+                         }
+                         &:hover {
+                              border-bottom: 2px solid blue; 
+                         }
+                         &:hover a{
+                              color: blue; 
+                         }
+                    }
+               
+               }
+          }
+     }
+
+</style>
